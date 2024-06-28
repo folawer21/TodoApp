e13a014612067f7c76dd7f2d01a6636cf4dd7f9c
folawer21 <folawer21@mail.ru>
2024-06-28 22:36:20 +0300

    colorAdded

---
+++ TodoApp/ColorExtension.swift
@@ -0,0 +1,8 @@
+//
+//  ColorExtension.swift
+//  TodoApp
+//
+//  Created by Александр  Сухинин on 28.06.2024.
+//
+
+import Foundation
---
+++ TodoApp/View/CustomColorPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomColorPickerView.swift
+//  TodoApp
+//
+//  Created by Александр  Сухинин on 28.06.2024.
+//
+
+import SwiftUI
+
+struct CustomColorPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomColorPickerView()
+}
