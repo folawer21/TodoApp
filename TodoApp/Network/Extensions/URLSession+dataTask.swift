a7be8efa12b51ec8e478c6eab8e5ca3bd89ebedc
folawer21 <folawer21@mail.ru>
2024-07-12 23:22:28 +0300

    remaked URLSession extension

--- TodoApp/Network/Extensions/URLSession+dataTask.swift
+++ TodoApp/Network/Extensions/URLSession+dataTask.swift
@@ -8,29 +8,37 @@
 import Foundation
 extension URLSession {
     func dataTask(for urlRequest: URLRequest) async throws -> (Data, URLResponse) {
-        return try await withCheckedThrowingContinuation { continuation in
-            let task = dataTask(with: urlRequest) { data, response, error in
-                if Task.isCancelled {
-                    continuation.resume(throwing: CancellationError())
-                    return
+        var task: URLSessionDataTask?
+        return try await withTaskCancellationHandler(
+            operation: {
+                try await withCheckedThrowingContinuation { continuation in
+                    let task = dataTask(with: urlRequest) { data, response, error in
+                        if Task.isCancelled {
+                            continuation.resume(throwing: CancellationError())
+                            return
+                        }
+                        if let error = error {
+                            continuation.resume(throwing: error)
+                            return
+                        }
+                        guard let data = data, let response = response else {
+                            continuation.resume(throwing: URLError(.badServerResponse))
+                            return
+                        }
+                        continuation.resume(returning: (data, response))
+                    }
+                    task.resume()
+                    Task {
+                        if Task.isCancelled {
+                            task.cancel()
+                            continuation.resume(throwing: CancellationError())
+                        }
+                    }
                 }
-                if let error = error {
-                    continuation.resume(throwing: error)
-                    return
-                }
-                guard let data = data, let response = response else {
-                    continuation.resume(throwing: URLError(.badServerResponse))
-                    return
-                }
-                continuation.resume(returning: (data, response))
-            }
-            task.resume()
-            Task {
-                if Task.isCancelled {
-                    task.cancel()
-                    continuation.resume(throwing: CancellationError())
-                }
-            }
-        }
+            },
+            onCancel: { [weak task] in
+                task?.cancel()
+            })
+        
     }
 }
